060f3a4f9836401dffe2a079ed3e72ae56cc358d
rohan-sarkarr <rohan180301@gmail.com>
2020-10-25 16:00:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_recommendations(title):
     return return_df
 
 # Set up the main route
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
 
 def main():
     if flask.request.method == 'GET':
